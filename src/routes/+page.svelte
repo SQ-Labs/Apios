a3b60ac029a810990db641e5621c7524d5a076b9
Georgia Bj\u00e4rst\u00e5l <georgia@blacklight.tv>
2022-11-19 22:57:12 +0100

    [ADD] template for future page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,16 @@
-<script>
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
-
 <svelte:head>
-	<title>APIOS</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
+    <title>APIOS</title>
+    <meta name="description" content="APIOS - SQ Labs" />
+    </svelte:head>
 
-<section>
-	<h1>APIOS
-	</h1>
-</section>
+<h1>APIOS</h1>
+<h2>Un titre</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget fermentum ligula. Suspendisse vitae dignissim massa. Nullam lacus quam, molestie at tellus sed, suscipit consectetur ligula. Etiam pharetra ex nibh, ac varius augue aliquet sed. Integer at euismod lacus. Nulla est est, efficitur vel augue vitae, lobortis maximus dui. Phasellus in viverra sem. Aliquam varius est vel felis ultrices, rutrum maximus enim lobortis. Morbi porta, turpis sit amet hendrerit pretium, nisi velit eleifend ex, nec molestie nibh lacus sed tortor. Etiam nulla dui, mollis at porta at, ultrices vel augue. Fusce feugiat ut nunc et lacinia.</p>
+<h3>Un titre</h3>
+<p>Mauris nec elit nisl. Duis sagittis felis sed urna dictum, vitae viverra enim ultricies. Nunc dignissim felis nec neque pellentesque, porttitor egestas augue ultrices. Nullam vestibulum non justo ut volutpat. Vestibulum posuere, ex non interdum rutrum, augue metus semper est, in rutrum nulla ipsum quis odio. Sed condimentum eleifend est, porttitor tristique sem ultrices eget. In convallis magna sed sollicitudin porttitor. Morbi lorem risus, facilisis quis iaculis nec, porttitor a lacus. Vestibulum id ante ut ligula finibus tristique id ac augue. Vestibulum eget tellus eu tellus posuere lobortis eu nec metus. Nullam accumsan fringilla tristique. Quisque suscipit quis ex ut feugiat.</p>
+<h3>Un titre</h3>
+<p>Cras placerat ex mollis ipsum fringilla laoreet dapibus et mauris. Aenean arcu felis, laoreet interdum purus sit amet, vestibulum pellentesque metus. Vestibulum ac vehicula sapien, eu placerat odio. Phasellus dolor ipsum, mollis a vulputate vitae, lacinia ac leo. Ut sit amet augue auctor, lacinia tortor vel, porta dolor. Integer et velit nec arcu dignissim vulputate. Mauris porta nunc risus, at hendrerit lectus finibus nec. Vestibulum velit ipsum, gravida sed arcu a, consectetur euismod sapien.</p>
+<h2>Un titre</h2>
+<p>Vestibulum ut urna eget nisl efficitur aliquet. In ut nisi dolor. Duis consectetur orci in magna vehicula, in molestie mi bibendum. Proin nec ullamcorper nibh. Aenean erat diam, placerat quis porttitor in, lobortis eget lectus. Sed quis lacus eget erat dapibus mattis in non nisi. Nullam turpis leo, mollis eu porttitor quis, feugiat et arcu. Nam vitae quam metus. Aenean semper, mi non sagittis dignissim, nibh sem placerat purus, quis tempor mi lacus dapibus nibh. Maecenas eu justo sed massa euismod iaculis. Proin egestas eros mi, ornare dignissim ante pretium ac. Sed pharetra nisl mauris, finibus auctor ex blandit vitae. Fusce et justo ligula. Proin malesuada tempus laoreet. Nullam id volutpat mauris. Proin lacus massa, ultricies convallis lectus nec, maximus consequat felis.</p>
+<p>Praesent in purus a quam pellentesque sagittis. Etiam bibendum iaculis vestibulum. Praesent at turpis nulla. Sed molestie ante neque, vel facilisis tellus pellentesque eget. Ut sit amet massa imperdiet, lacinia tortor a, finibus nisl. Aliquam ex quam, consectetur non posuere quis, blandit eget risus. Duis ut dui id elit convallis tempus at non magna. Donec vitae mollis nisl. Suspendisse potenti. </p>
 
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-</style>
