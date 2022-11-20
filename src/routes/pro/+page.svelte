c3641fa01741f262bbe57961598c4712d3ea3be3
Stephane Gelibert <stephane.gelibert@epita.fr>
2022-11-20 11:27:21 +0100

    fix: changed default font and fixed typo

--- src/routes/pro/+page.svelte
+++ src/routes/pro/+page.svelte
@@ -9,12 +9,12 @@ let password = ""
 </script>
 
 <div class="text-column">
-	<h1>Bon retour parmis nous !</h1>
+	<h1>Bon retour parmi nous !</h1>
 
 <input type="text" placeholder="Login" bind:value={login} />
 	<input type="password" placeholder="Mot de passe" bind:value={password} />
 	<a href="pro/search">
 
-<button>Se Connecter</button>
+<button>Se connecter</button>
 	</a>
 </div>
